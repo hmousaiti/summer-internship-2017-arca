9dc4cd96ae8274e386cb1e13a38071d8433d71d4
hasamad <hasamad@eg.ibm.com>
2017-08-09 15:57:23 +0200

    Fixes for find my car

--- arca-web/src/main/webapp/app/modules/find-my-car/find-my-car-controller.js
+++ arca-web/src/main/webapp/app/modules/find-my-car/find-my-car-controller.js
@@ -21,29 +21,25 @@ function FindMyCarController($scope, $http) {
 		$scope.found = "Something went wrong";
 		lon= 50.0192871;
 		latt=50.0752169;
-		//myMap();
 		myMap();
 		});
 	
 	$scope.GetPath = function() {
 		TakeTime();
-		//for(var i in json_data)
+		
 		    
 		$http.get("rest/location/path?from="+from+"&to="+to)
 		.then(function(response) {
 			var jsonData = response.data.path;
-			//document.getElementById("display").innerHTML = jsonData.path[1].long;
 			for(var i = 0; i< jsonData.length ; i++)
 				{
-					locations.push([i, jsonData[i]]);
-				//	document.getElementById("display").innerHTML = jsonData.path[i];
+					locations.push(jsonData[i]);
 				}
 			initialize();
-			document.getElementById("display").innerHTML = locations[0][1].lat;
 			$scope.location = "Requested Path";
 		},function(response) {
 			//Second function handles error
-//			alert("SMS Failed !");
+
 		});
 		
 	}
@@ -51,13 +47,13 @@ function FindMyCarController($scope, $http) {
 	$scope.SendSMS = function() {
 		
 		var phone = document.getElementById("phone").value;
-		$http.get("https://arca-location-webservice.mybluemix.net/location?lat="+latt+"&lng="+lon+"number="+phone)
+		$http.get("https://arca-location-webservice.mybluemix.net/location?lat="+latt+"&lng="+lon+"&number="+phone)
 		.then(function(response) {
-//			alert("SMS Sent  !");
+			alert("SMS Sent  !");
 				
 		},function(response) {
 			//Second function handles error
-//			alert("SMS Failed !");
+			alert("SMS Failed !");
 			});
 		
 	}
@@ -67,56 +63,67 @@ function FindMyCarController($scope, $http) {
 	  directionsDisplay = new google.maps.DirectionsRenderer();
 	  var directionsDisplay;
 	  var directionsService = new google.maps.DirectionsService();
-//	  var locations = [
-//	    ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-//	    ['Bondi Beach', -33.890542, 151.274856, 4],
-//	    ['Coogee Beach', -33.923036, 151.259052, 5],
-//	    ['Maroubra Beach', -33.950198, 151.259302, 1],
-//	    ['Cronulla Beach', -34.028249, 151.157507, 3]
-//	  ];
-
 	  var map = new google.maps.Map(document.getElementById('googleMap'), {
-	    zoom: 10,
-	    center: new google.maps.LatLng(latt, lon),
+	    zoom: 14,
+	    center: new google.maps.LatLng(locations[locations.length-1].lat, locations[locations.length-1].long),
 	    mapTypeId: google.maps.MapTypeId.ROADMAP
 	  });
 	  directionsDisplay.setMap(map);
 	  var infowindow = new google.maps.InfoWindow();
 
-	  var marker, i;
-	  var request = {
-	    travelMode: google.maps.TravelMode.DRIVING
-	  };
-	  for (i = 0; i < locations.length; i++) {
-	    marker = new google.maps.Marker({
-	      position: new google.maps.LatLng(locations[i][1].lat, locations[i][1].long),
+	  	  
+	   marker1 = new google.maps.Marker({
+	      position: new google.maps.LatLng(locations[0].lat, locations[0].long),
+	      
+	      map : map
 	    });
+	   marker2 = new google.maps.Marker({
+	      position: new google.maps.LatLng(locations[locations.length -1].lat, locations[locations.length -1].long),
+	      icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
+	      map : map
+	    });
+	  
+	  
+	  for (var i = 0, parts = [], max = 25 ; i < locations.length; i = i + max)
+	        parts.push(locations.slice(i, i + max + 1));
 
-	    google.maps.event.addListener(marker, 'click', (function(marker, i) {
-	      return function() {
-	        infowindow.setContent(locations[i][0]);
-	        infowindow.open(map, marker);
-	      }
-	    })(marker, i));
+	    // Service callback to process service results
+	    var service_callback = function(response, status) {
+	        if (status != 'OK') {
+	            console.log('Directions request failed due to ' + status);
+	            return;
+	        }
+	        var renderer = new google.maps.DirectionsRenderer;
+	        renderer.setMap(map);
+	        renderer.setOptions({ suppressMarkers: true, preserveViewport: true });
+	        renderer.setDirections(response);
+	    };
 
-	    if (i == 0) request.origin = marker.getPosition();
-	    else if (i == locations.length - 1) request.destination = marker.getPosition();
-	    else {
-	      if (!request.waypoints) request.waypoints = [];
-	      request.waypoints.push({
-	        location: marker.getPosition(),
-	        stopover: true
-	      });
+	    // Send requests to service to get route (for stations count <= 25 only one request will be sent)
+	    for (var i = 0; i < parts.length; i++) {
+	        // Waypoints does not include first station (origin) and last station (destination)
+	        var waypoints = [];
+	        var pos;
+	        for (var j = 1; j < parts[i].length - 1; j++)
+	        	pos =	new google.maps.LatLng(parts[i][j].lat, parts[i][j].long);
+	            waypoints.push({location: pos, stopover: true});
+	        // Service options
+	        var posStart =	new google.maps.LatLng(parts[i][0].lat, parts[i][0].long);
+	        var posEnd =	new google.maps.LatLng(parts[i][parts[i].length - 1].lat, parts[i][parts[i].length - 1].long);
+	        var service_options = {
+	        	origin:  posEnd,
+	            destination: posStart,
+	            waypoints: waypoints,
+	            travelMode: 'DRIVING'
+	        };
+	        // Send request
+	        directionsService.route(service_options, service_callback);
 	    }
 
-	  }
-	  directionsService.route(request, function(result, status) {
-	    if (status == google.maps.DirectionsStatus.OK) {
-	      directionsDisplay.setDirections(result);
-	    }
-	  });
+
+	  locations = [];
 	}
-	//google.maps.event.addDomListener(window, "load", initialize);
+	
 
 	function TakeTime()
 	{
@@ -124,7 +131,6 @@ function FindMyCarController($scope, $http) {
 		var time = x.value;
 		var myDate = new Date(time);
 		from = myDate.getTime();
-		//document.getElementById("display").innerHTML = n;
 		x = document.getElementById("to");
 		time = x.value;
 		myDate = new Date(time);
@@ -133,9 +139,8 @@ function FindMyCarController($scope, $http) {
    
 
 function myMap() {
-		//directionsDisplay = new google.maps.DirectionsRenderer();
+		
 		var mapProp= {
-		    //center:new google.maps.LatLng(30.0752169,31.0192871),
 		    zoom: 15,
 		    center: {lat: latt, lng: lon}
 		};
